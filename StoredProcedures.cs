fdb3430b386360da24a6bda0e1578d7ad635af16
quckly <me@quckly.ru>
2017-03-30 12:50:54 +0500

    First release.

---
+++ StoredProcedures.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using System.Data.Odbc;
+using System.Net;
+using System.Text;
+
+namespace SqlServerPostgresIntegration
+{
+    public partial class StoredProcedures
+    {
+        //public static void ExecutePgsql(OdbcCommand cmd)
+        //{
+        //    using (var conn = new OdbcConnection("DSN=PostgreSQL30;")) //"Driver={PostgreSQL ANSI};Server=localhost;Port=5432;Database=db5;Uid=postgres;Pwd=postgres;")) // PostgreSQL30 // DEVARTPG
+        //    {
+        //        //throw new Exception(cmd.CommandText);
+        //        conn.Open();
+        //        cmd.Connection = conn;
+        //        cmd.ExecuteNonQuery();
+        //    }
+        //}
+
+        [SqlTrigger(Name = @"ReplicaToPg", Target = "db5", Event = "AFTER INSERT, UPDATE, DELETE")]
+        public static void ReplicaToPg()
+        {
+            SqlCommand command;
+            SqlTriggerContext triggContext = SqlContext.TriggerContext;
+            SqlDataReader reader;
+
+            // Retrieve the connection that the trigger is using  
+            using (SqlConnection connection = new SqlConnection(@"context connection=true"))
+            {
+                connection.Open();
+
+                SqlCommand cmdTableName =
+                    new SqlCommand(
+                        "SELECT object_name(resource_associated_entity_id) FROM sys.dm_tran_locks WHERE request_session_id = @@spid and resource_type = 'OBJECT'",
+                        connection);
+                var tableName = cmdTableName.ExecuteScalar().ToString();
+
+                switch (triggContext.TriggerAction)
+                {
+                    case TriggerAction.Insert:
+                        {
+                            command = new SqlCommand(@"SELECT * FROM INSERTED;", connection);
+                            reader = command.ExecuteReader();
+
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    StringBuilder sb = new StringBuilder($"INSERT INTO {tableName} (");
+
+                                    for (int columnNumber = 0; columnNumber < triggContext.ColumnCount; columnNumber++)
+                                    {
+                                        if (columnNumber > 0)
+                                        {
+                                            sb.Append(", ");
+                                        }
+
+                                        sb.Append(reader.GetName(columnNumber));
+                                    }
+                                    sb.Append(") VALUES (");
+
+                                    for (int columnNumber = 0; columnNumber < triggContext.ColumnCount; columnNumber++)
+                                    {
+                                        if (columnNumber > 0)
+                                        {
+                                            sb.Append(", ");
+                                        }
+
+                                        //sb.Append("?");
+                                        if (reader.GetDataTypeName(columnNumber) == "int")
+                                        {
+                                            sb.Append(
+                                                $"{reader.GetSqlValue(columnNumber).ToString()}");
+                                        }
+                                        else
+                                        {
+                                            sb.Append(
+                                                $"'{reader.GetSqlValue(columnNumber).ToString().Replace("'", "''")}'");
+                                        }
+                                    }
+
+                                    sb.Append(")");
+
+                                    //using (var cmd = new OdbcCommand(sb.ToString()))
+                                    //{
+                                    //    for (int columnNumber = 0; columnNumber < triggContext.ColumnCount; columnNumber++)
+                                    //    {
+                                    //        var param = new OdbcParameter();
+                                    //        param.OdbcType = (OdbcType)Enum.Parse(typeof(OdbcType), reader.GetDataTypeName(columnNumber), true);
+                                    //        param.Value = reader.GetSqlValue(columnNumber);
+                                    //        cmd.Parameters.Add(param);
+                                    //    }
+
+                                    //    ExecutePgsql(cmd);
+                                    //}
+
+                                    WebClient client = new WebClient();
+                                    client.UploadString("http://192.168.1.10:8080/", sb.ToString());
+                                }
+
+                                reader.Close();
+                            }
+
+                            break;
+                        }
+
+                    case TriggerAction.Update:
+                        {
+                            command = new SqlCommand(@"SELECT * FROM INSERTED;", connection);
+                            reader = command.ExecuteReader();
+                            reader.Read();
+
+                            for (int columnNumber = 0; columnNumber < triggContext.ColumnCount; columnNumber++)
+                            {
+                                //pipe.Send("Updated column "
+                                //            + reader.GetName(columnNumber) + "? "
+                                //            + triggContext.IsUpdatedColumn(columnNumber).ToString());
+                            }
+
+                            reader.Close();
+
+                            break;
+                        }
+
+                    case TriggerAction.Delete:
+                        {
+                            command = new SqlCommand(@"SELECT * FROM DELETED;", connection);
+                            reader = command.ExecuteReader();
+
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+
+                                }
+
+                                reader.Close();
+                            }
+
+                            break;
+                        }
+                }
+            }
+        }
+
+        [Microsoft.SqlServer.Server.SqlProcedure]
+        public static void HelloWorld()
+        {
+            Microsoft.SqlServer.Server.SqlMetaData columnInfo
+                    = new Microsoft.SqlServer.Server.SqlMetaData("Column1", SqlDbType.NVarChar, 12);
+            SqlDataRecord greetingRecord
+                = new SqlDataRecord(new Microsoft.SqlServer.Server.SqlMetaData[] { columnInfo });
+            greetingRecord.SetString(0, "Hello world!");
+            SqlContext.Pipe.Send(greetingRecord);
+        }
+    };
+}
---
+++ TestPgOdbc/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Data.Odbc;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TestPgOdbc
+{
+    class Program
+    {
+        public static void ExecutePgsql(OdbcCommand cmd)
+        {
+            try
+            {
+                using (var conn = new OdbcConnection("DSN=PostgreSQL35W;"))
+                    //"Driver={PostgreSQL ANSI};Server=localhost;Port=5432;Database=db5;Uid=postgres;Pwd=postgres;")) // PostgreSQL30 // DEVARTPG
+                {
+                    conn.Open();
+                    cmd.Connection = conn;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        public static void Run()
+        {
+            var web = new HttpListener();
+
+            web.Prefixes.Add("http://*:8080/");
+            web.Start();
+            
+            while (web.IsListening)
+            {
+                var context = web.GetContext();
+
+                var query = ReadStringRequest(context.Request).Result;
+
+                using (var cmd = new OdbcCommand(query))
+                {
+                    ExecutePgsql(cmd);
+                }
+
+                context.Response.ContentType = "text/html";
+                context.Response.StatusCode = 200;
+
+                context.Response.OutputStream.Close();
+            }
+        }
+
+        public static async Task<string> ReadStringRequest(HttpListenerRequest request)
+        {
+            if (request.InputStream.CanRead)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    await request.InputStream.CopyToAsync(ms);
+
+                    return Encoding.UTF8.GetString(ms.ToArray());
+                }
+            }
+
+            return null;
+        }
+
+        //public static void Run()
+        //{
+        //    var listener = new TcpListener(IPAddress.Any, 8080);
+        //    listener.Start();
+        //    while (true)
+        //    {
+        //        byte[] buff = new byte[10024];
+        //        MemoryStream ms = new MemoryStream();
+        //        TcpClient s = listener.AcceptTcpClient();
+
+        //        int Count;
+        //        //while ((Count = s.GetStream().Read(buff, 0, buff.Length)) > 0)
+        //        Count = s.GetStream().Read(buff, 0, buff.Length);
+        //        {
+        //            ms.Write(buff, 0, Count);
+        //        }
+
+        //        var httpRequest = Encoding.UTF8.GetString(ms.ToArray());
+        //        var id = httpRequest.IndexOf("\r\n\r\n");
+        //        var query = httpRequest.Substring(id + 4);
+
+        //        using (var cmd = new OdbcCommand(query))
+        //        {
+        //            ExecutePgsql(cmd);
+        //        }
+        //    }
+        //}
+    }
+}
